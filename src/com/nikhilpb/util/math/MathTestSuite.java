9987ccb2cddfdec2a6fffa8bd9180ff2bba7f966
Nikhil Bhat <nikhil.p.b@gmail.com>
2013-10-15 02:23:03 -0400

    psd matrix added

---
+++ src/com/nikhilpb/util/math/MathTestSuite.java
@@ -0,0 +1,17 @@
+package com.nikhilpb.util.math;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nikhilpb
+ * Date: 10/15/13
+ * Time: 12:46 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@RunWith(value = Suite.class)
+@Suite.SuiteClasses(value = { DistributionsTest.class,
+        PSDMatrixTest.class,
+        RegressionTest.class })
+public class MathTestSuite { }
--- src/com/nikhilpb/util/math/PSDMatrixTest.java
+++ src/com/nikhilpb/util/math/PSDMatrixTest.java
@@ -15,21 +15,19 @@ import java.io.File;
 public class PSDMatrixTest {
     private static final double kTol = 1E-4;
     private static final String kFilename = "/tmp/psdtest-file";
+
     @Test
     public void testBase() throws Exception {
-        double[][] psdMatArray = {{2.,0.},{0.,1.}};
+        double[][] psdMatArray = {{2.,0.1},{0.1,1.}};
         PSDMatrix psdMatrix = new PSDMatrix(new Matrix(psdMatArray));
         Matrix sqrt = psdMatrix.sqrt();
         Matrix sqrtt = sqrt.transpose();
         Matrix matrix = sqrt.times(sqrtt);
         assert Math.abs(matrix.minus(new Matrix(psdMatArray)).normF()) < kTol;
         psdMatrix.writeToFile(kFilename);
-
         PSDMatrix psdMatrix1 = new PSDMatrix(kFilename);
-
         assert psdMatrix.approxEqual(psdMatrix, kTol);
         assert psdMatrix.approxEqual(psdMatrix1, kTol);
-
         File file = new File(kFilename);
         assert file.delete();
     }
