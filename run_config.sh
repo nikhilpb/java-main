b795e7c8a092501e54b4baccda5a16b6f8e35618
Nikhil Bhat <nbhat15@gsb.columbia.edu>
2013-10-27 19:37:54 -0400

    scripting

--- run_config.sh
+++ run_config.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-CONFIG=${1:-"config/config.xml"}
+JAVA=java
+CONFIG=${2:-"config/config.xml"}
+MEMORY="6g"
 
-java -Xmx6g -Djava.library.path=$CPLEXJAVALIBDIR \
-    	-classpath classes:$CPLEXJARDIR:lib/commons-collections-3.2.1.jar:lib/jakarta-oro-2.0.8.jar \
-    	com.nikhilpb.matching.MatchingMain $CONFIG
+FLAGS="-Xmx"$MEMORY" -Djava.library.path="$CPLEXJAVALIBDIR
+LIBS=$CPLEXJARDIR":lib/commons-collections-3.2.1.jar:lib/jakarta-oro-2.0.8.jar"
+
+case "$1" in 
+"matching") MAINCLASS="com.nikhilpb.matching.MatchingMain"
+		;;
+"stopping") MAINCLASS="com.nikhilpb.stopping.StoppingMain"
+		;;
+*) 	echo "no class corresponding to "$1
+		exit
+		;;
+esac
+
+$JAVA $FLAGS -classpath classes:$LIBS $MAINCLASS $CONFIG
