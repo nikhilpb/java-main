1d7c6b09d31e5dfacbe1b78bd272e439e60e3a2e
Nikhil Bhat <nikhil.p.b@gmail.com>
2013-10-27 20:51:00 -0400

    rearranging config

--- run_config.sh
+++ run_config.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 JAVA=java
-CONFIG=${2:-"config/config.xml"}
-MEMORY="6g"
+CONFIGBASE="config/"
+CONFIG=${2:-"main.xml"}
+MEMORY="2g"
 
 FLAGS="-Xmx"$MEMORY" -Djava.library.path="$CPLEXJAVALIBDIR
 LIBS=$CPLEXJARDIR":lib/commons-collections-3.2.1.jar:lib/jakarta-oro-2.0.8.jar"
 
 case "$1" in 
 "matching") MAINCLASS="com.nikhilpb.matching.MatchingMain"
+            CONFIG=$CONFIGBASE"matching/"$CONFIG
 		;;
 "stopping") MAINCLASS="com.nikhilpb.stopping.StoppingMain"
+            CONFIG=$CONFIGBASE"stopping/"$CONFIG
 		;;
 *) 	echo "no class corresponding to "$1
 		exit
