329c4a4eae63abd03aee486768d2dd818b9b4ed5
Nikhil Bhat <nikhil.p.b@gmail.com>
2013-11-03 14:02:09 -0500

    QFunction added

---
+++ src/com/nikhilpb/adp/QFunction.java
@@ -0,0 +1,12 @@
+package com.nikhilpb.adp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nikhilpb
+ * Date: 11/3/13
+ * Time: 1:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface QFunction {
+    public double value(State state, Action action);
+}
---
+++ src/com/nikhilpb/adp/QFunctionPolicy.java
@@ -0,0 +1,42 @@
+package com.nikhilpb.adp;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nikhilpb
+ * Date: 11/3/13
+ * Time: 1:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class QFunctionPolicy implements Policy {
+    private MarkovDecisionProcess model;
+    private QFunction qFunction;
+    private RewardFunction rewardFunction;
+    private double alpha;
+
+    public QFunctionPolicy(MarkovDecisionProcess model,
+                           QFunction qFunction,
+                           RewardFunction rewardFunction,
+                           double alpha) {
+        this.model = model;
+        this.qFunction = qFunction;
+        this.rewardFunction = rewardFunction;
+        this.alpha = alpha;
+    }
+
+    @Override
+    public Action getAction(State state) {
+        ArrayList<Action> actions = state.getActions();
+        Action maxAction = null;
+        double value, maxValue = Double.MIN_VALUE;
+        for (Action a : actions) {
+            value = rewardFunction.value(state, a) + alpha * qFunction.value(state, a);
+            if (value > maxValue) {
+                maxValue = value;
+                maxAction = a;
+            }
+        }
+        return maxAction;
+    }
+}
