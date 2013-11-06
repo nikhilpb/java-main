0d8b8e063319c54c78b581adba28116a33da806b
Nikhil Bhat <nbhat15@gsb.columbia.edu>
2013-11-06 14:37:21 -0500

    memory option added

--- run_config.sh
+++ run_config.sh
@@ -3,7 +3,7 @@
 JAVA=java
 CONFIGBASE="config/"
 CONFIG=${2:-"main"}
-MEMORY="2g"
+MEMORY=${3:-"2g"}
 
 FLAGS="-Xmx"$MEMORY" -Djava.library.path="$CPLEXJAVALIBDIR
 LIBS=$CPLEXJARDIR":lib/commons-collections-3.2.1.jar:lib/jakarta-oro-2.0.8.jar:lib/Jama-1.0.1.jar"
