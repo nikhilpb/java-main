2732cd11c0ec5c1a8101648f4844faa6fb890e47
Nikhil Bhat <nikhil.p.b@gmail.com>
2013-11-06 14:32:47 -0500

    protobuf added

--- run_config.sh
+++ run_config.sh
@@ -6,7 +6,7 @@ CONFIG=${2:-"main"}
 MEMORY="2g"
 
 FLAGS="-Xmx"$MEMORY" -Djava.library.path="$CPLEXJAVALIBDIR
-LIBS=$CPLEXJARDIR":lib/commons-collections-3.2.1.jar:lib/jakarta-oro-2.0.8.jar:lib/Jama-1.0.1.jar"
+LIBS=$CPLEXJARDIR":lib/commons-collections-3.2.1.jar:lib/jakarta-oro-2.0.8.jar:lib/Jama-1.0.1.jar:lib/protobuf-java-2.5.0.jar"
 
 case "$1" in 
 "matching") MAINCLASS="com.nikhilpb.matching.MatchingMain"
@@ -20,4 +20,5 @@ case "$1" in
 		;;
 esac
 
+echo "$JAVA $FLAGS -classpath classes:$LIBS $MAINCLASS $CONFIG"
 $JAVA $FLAGS -classpath classes:$LIBS $MAINCLASS $CONFIG
