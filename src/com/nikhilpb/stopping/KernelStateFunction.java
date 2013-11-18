78ab581d98efa61bfc2d6c6abb5c09e60de27556
Nikhil Bhat <nikhil.p.b@gmail.com>
2013-11-18 14:58:23 -0500

    some debugging

--- src/com/nikhilpb/stopping/KernelStateFunction.java
+++ src/com/nikhilpb/stopping/KernelStateFunction.java
@@ -42,7 +42,7 @@ public class KernelStateFunction implements StateFunction {
 
     @Override
     public double value(State state) {
-        double value = -b;
+        double value = b;
         StoppingState stoppingState = (StoppingState)state;
         for (int i = 0; i < nextStates.size(); ++i) {
             StoppingState nState = nextStates.get(i);
