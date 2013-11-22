1ddf9baee2e759827205214bbf9eaa272087067a
Nikhil Bhat <nikhil.p.b@gmail.com>
2013-11-21 21:07:35 -0500

    config changes

---
+++ pools-offline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+problem_size=${1:-100}
+sample_runs=300
+simvalue_seed=1
+
+java -Xmx6g -Djava.library.path=$CPLEXJAVALIBDIR \
+    -classpath classes:$CPLEXJARDIR:lib/commons-collections-3.2.1.jar:lib/jakarta-oro-2.0.8.jar \
+    com.nikhilpb.pools.MatchingPoolsMain \
+	model data/pools-model.txt \; \
+	value greedy $sample_runs $problem_size $simvalue_seed
