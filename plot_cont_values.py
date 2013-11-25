208a4d7ee02bdcd5adfcf130e47730b405ff5cc1
Nikhil Bhat <nbhat15@gsb.columbia.edu>
2013-11-25 16:30:05 -0500

    plotting cont values

--- plot_cont_values.py
+++ plot_cont_values.py
@@ -1,10 +1,29 @@
-#!/usr/bin/python
+#!/apps/gurobi/linux64/bin/python2.7
 
 import sys
 
 if len(sys.argv) != 3:
 	sys.exit("expect 2 arguments")	
 
-file = 'results/{0}'.format(sys.argv[1])
+file_name = 'results/' + sys.argv[1]
 
-print file
+time_periods = int(sys.argv[2])
+
+print 'plotting continuation values'
+print 'file name: ' + file_name + ', time perids: ' + str(time_periods)
+
+import csv
+
+file = open(file_name, 'rb')
+reader = csv.reader(file)
+
+data = {}
+
+for r in reader:
+	if r[0] in data:
+		data[r[0]].append([float(r[1]), float(r[2])])
+	else:
+		data[r[0]] = []
+
+print data
+
